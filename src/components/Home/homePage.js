dbdee5d664a020e481533ef012ec7aa79bd17005
unknown <maxime.soufer@gmail.com>
2022-04-15 08:23:07 -0400

    add minor loading tweak

--- src/components/Home/homePage.js
+++ src/components/Home/homePage.js
@@ -6,17 +6,25 @@ import Bulletin from "./bulletin";
 import Footer from "../Global/footer";
 import LoadingScreen from "../Global/loadingScreen";
 
-import "./styles/home-page.css"
+import "./styles/home-page.css";
 
 export default function HomePage() {
-  
   const [loadingID, setLoadingID] = useState("");
   const [gridID, setGridID] = useState("hidden");
 
   useEffect(() => {
-    setTimeout(() => setLoadingID("hidden"), 3000);
-    setTimeout(() => setGridID(""), 3000);
+    if (document.readyState === "complete") {
+      completeLoading();
+    } else {
+      window.addEventListener("load", completeLoading);
+    }
   });
+
+  function completeLoading() {
+    setTimeout(() => setLoadingID("hidden"), 2000);
+    setTimeout(() => setGridID(""), 2000);
+    window.removeEventListener("load", completeLoading);
+  }
 
   return (
     <>
